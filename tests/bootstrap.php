f225578803dddb40a75ad0cd64c344752b7c3442
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2025-12-15 22:51:24 +0100

    Update phpunit/phpunit recipe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,4 +15,4 @@ if (file_exists(\dirname(__DIR__) . '/config/bootstrap.php')) {
     (new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
 }
 
-(new Symfony\Component\Filesystem\Filesystem())->remove(__DIR__ . '/../var/cache/test');
+new Symfony\Component\Filesystem\Filesystem()->remove(__DIR__ . '/../var/cache/test');
